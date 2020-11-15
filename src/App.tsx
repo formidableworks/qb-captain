2e21596099c6aae66780f62231820192b49edda2
admin <admin@formidableworks.com>
2020-11-15 15:24:03 +0000

    file cleanup: create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import './App.css';
 import { LoginForm } from './features/authentication/LoginForm';
 import { MainDataControl } from './features/sync/MainDataControl';
-import logo from './logo.svg';
 
 export function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <LoginForm />
-        <MainDataControl />
-      </header>
+    <div>
+      <LoginForm />
+      <MainDataControl />
     </div>
   );
 }
